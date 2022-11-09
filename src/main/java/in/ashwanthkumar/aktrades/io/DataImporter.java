f3c8e58a81ed927334003790d8b3130b4b0ea9d4
ashwanthkumar <ashwanthkumar@googlemail.com>
2022-11-09 06:00:25 +0530

    Persisting only the columns we actually need and using smaller dictionary indices for our use-case

--- src/main/java/in/ashwanthkumar/aktrades/io/DataImporter.java
+++ src/main/java/in/ashwanthkumar/aktrades/io/DataImporter.java
@@ -7,8 +7,8 @@ import org.apache.arrow.vector.dictionary.Dictionary;
 import org.apache.arrow.vector.dictionary.DictionaryEncoder;
 import org.apache.arrow.vector.dictionary.DictionaryProvider;
 import org.apache.arrow.vector.holders.NullableFloat4Holder;
-import org.apache.arrow.vector.holders.NullableFloat8Holder;
 import org.apache.arrow.vector.ipc.ArrowFileWriter;
+import org.apache.arrow.vector.types.pojo.ArrowType;
 import org.apache.arrow.vector.types.pojo.DictionaryEncoding;
 import org.apache.arrow.vector.types.pojo.Field;
 import org.apache.arrow.vector.util.Text;
@@ -29,7 +29,6 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class DataImporter {
@@ -64,56 +63,27 @@ public class DataImporter {
             t.addColumns(parsedDateTime);
             t.removeColumns("Date/Time");
             Predicate<LocalDateTime> withinFnOTimeRange = localDateTime -> localDateTime.getHour() < 15 || (localDateTime.getHour() == 15 && localDateTime.getMinute() <= 29);
-            Selection filterRowsBeyondFnOClose = parsedDateTime.eval(withinFnOTimeRange.negate());
-            t = t.dropWhere(filterRowsBeyondFnOClose);
+            Selection filterRowsBeyondFnOClose = parsedDateTime.eval(withinFnOTimeRange);
 
-            // Step 2: Compute the underlying and remove the rows where the underlying is null
+            // Step 2: Keep only the required tickers for now: NF and BNF
             StringColumn tickerCol = t.column("Ticker").asStringColumn();
-            StringColumn underlyingCol = StringColumn.create("Underlying", tickerCol.size());
-            tickerCol.mapInto(ticker -> {
+            Predicate<String> isNfOrBnf = ticker -> {
                 Set<String> bnfIndex = Set.of("BANKNIFTY", "BANKNIFTY-FUT");
                 Set<String> nfIndex = Set.of("NIFTY", "NIFTY-FUT");
-                if (nfIndex.contains(ticker) || StringUtils.startsWith(ticker, "NIFTYWK")) {
-                    return "NF";
-                } else if (bnfIndex.contains(ticker) || StringUtils.startsWith(ticker, "BANKNIFTYWK")) {
-                    return "BNF";
-                }
-                return NA;
-            }, underlyingCol);
-            Predicate<String> isNA = input -> StringUtils.equals(input, NA);
-            Selection removeNAs = underlyingCol.eval(isNA);
-            t.addColumns(underlyingCol);
-            t = t.dropWhere(removeNAs);
 
-            // Step 3: Parse Option Strikes and Types
-            tickerCol = t.column("Ticker").asStringColumn();
-            Pattern optionTickerPattern = Pattern.compile("NIFTYWK([0-9]+)(CE|PE)");
+                return nfIndex.contains(ticker) || StringUtils.startsWith(ticker, "NIFTYWK") ||
+                        bnfIndex.contains(ticker) || StringUtils.startsWith(ticker, "BANKNIFTYWK");
+            };
+            Selection keepOnlyNfAndBnf = tickerCol.eval(isNfOrBnf);
 
-            StringColumn optionType = StringColumn.create("OptionType", tickerCol.size());
-            tickerCol.mapInto(ticker -> optionTickerPattern.matcher(ticker)
-                    .results()
-                    .map(mr -> mr.group(2))
-                    .findFirst()
-                    .orElse(null), optionType);
-            t.addColumns(optionType);
-
-            IntColumn optionStrike = IntColumn.create("OptionStrike", tickerCol.size());
-            tickerCol.mapInto(ticker -> optionTickerPattern.matcher(ticker)
-                    .results()
-                    .map(mr -> mr.group(1))
-                    .findFirst()
-                    .map(Integer::parseInt)
-                    .orElse(null), optionStrike);
-            t.addColumns(optionStrike);
-
-            return t;
+            // return a subset of table that matches all the required selections
+            return t.where(keepOnlyNfAndBnf.and(filterRowsBeyondFnOClose));
         });
         Table table = dataImporter.getTable();
         System.out.println(table.summary());
 
-        // we use dictionary to encode them to save space
-        // Also these should only be STRING column types
-        Set<String> categoricalColumns = Set.of("Ticker", "Underlying", "OptionType");
+        // we use dictionary to encode these less cardinality columns to save space.
+        Set<String> categoricalColumns = Set.of("Ticker");
 
         AtomicInteger dictId = new AtomicInteger(1);
         try (BufferAllocator allocator = new RootAllocator()) {
@@ -124,19 +94,15 @@ public class DataImporter {
                 if (ColumnType.valueOf(column.type().name()).equals(ColumnType.DOUBLE)) {
                     Float4Vector arrowColumn = new Float4Vector(column.name(), allocator);
                     arrowColumn.allocateNew(column.size());
-                    NullableFloat4Holder nullableValue = new NullableFloat4Holder();
                     for (int i = 0; i < column.size(); i++) {
                         Double fromRow = (Double) column.get(i);
                         if (fromRow == null) {
-                            nullableValue.isSet = 0;
+                            arrowColumn.setNull(i);
                         } else {
-                            nullableValue.isSet = 1;
-                            nullableValue.value = fromRow.floatValue();
+                            arrowColumn.set(i, fromRow.floatValue());
                         }
-                        arrowColumn.set(i, nullableValue);
                     }
                     arrowColumn.setValueCount(column.size());
-                    arrowColumn.reAlloc();
                     valueVectors.add(arrowColumn);
                 } else if (ColumnType.valueOf(column.type().name()).equals(ColumnType.STRING)) {
                     boolean isCategorical = categoricalColumns.contains(column.name());
@@ -145,15 +111,13 @@ public class DataImporter {
                     dictVector.allocateNew();
                     if (isCategorical) {
                         StringColumn uniqueValues = (StringColumn) column.unique();
-                        uniqueValues.sortAscending();
                         for (int i = 0; i < uniqueValues.size(); i++) {
                             dictVector.set(i, new Text(uniqueValues.getString(i)));
                         }
                         dictVector.setValueCount(uniqueValues.size());
                     }
-                    Dictionary catDictionary = new Dictionary(dictVector, new DictionaryEncoding(dictId.getAndIncrement(), true, /*indexType=*/null));
+                    Dictionary catDictionary = new Dictionary(dictVector, new DictionaryEncoding(dictId.getAndIncrement(), false, new ArrowType.Int(16, false)));
                     dictProvider.put(catDictionary);
-
 
                     VarCharVector arrowColumn = new VarCharVector(column.name(), allocator);
                     double numberOfBytes = column
@@ -172,7 +136,7 @@ public class DataImporter {
                     arrowColumn.setValueCount(column.size());
 
                     if (isCategorical) {
-                        IntVector encodedVector = (IntVector) DictionaryEncoder.encode(arrowColumn, catDictionary);
+                        UInt2Vector encodedVector = (UInt2Vector) DictionaryEncoder.encode(arrowColumn, catDictionary);
                         valueVectors.add(encodedVector);
                         arrowColumn.clear();
                     } else {
@@ -193,7 +157,7 @@ public class DataImporter {
                     arrowColumn.setValueCount(column.size());
                     valueVectors.add(arrowColumn);
                 } else if (ColumnType.valueOf(column.type().name()).equals(ColumnType.LOCAL_DATE_TIME)) {
-                    BigIntVector arrowColumn = new BigIntVector(column.name(), allocator);
+                    TimeStampSecVector arrowColumn = new TimeStampSecVector(column.name(), allocator);
                     arrowColumn.allocateNew(column.size());
                     for (int i = 0; i < column.size(); i++) {
                         LocalDateTime row = (LocalDateTime) column.get(i);
@@ -207,6 +171,7 @@ public class DataImporter {
                     valueVectors.add(arrowColumn);
                 }
             }
+
             List<Field> fields = valueVectors.stream().map(ValueVector::getField).collect(Collectors.toList());
             try (VectorSchemaRoot root = new VectorSchemaRoot(fields, valueVectors, table.rowCount());
                  FileOutputStream out = new FileOutputStream("output-foo.bin");
@@ -219,14 +184,10 @@ public class DataImporter {
                 throw new RuntimeException(e);
             }
 
+            // this is very important, we have to manually deallocate the memory in Root Allocator
+            // else it would result in memory leaks. This is because these are memory allocated
+            // outside the JVM.
             allocator.releaseBytes(allocator.getAllocatedMemory());
-            allocator.releaseBytes(allocator.getInitReservation());
-            // Clear the field vectors so the RootAllocator doesn't throw when closing
-            for (FieldVector column : valueVectors) {
-                column.clear();
-            }
-            valueVectors.clear();
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
